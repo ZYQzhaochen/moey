e4e9a2b2a6f20618e8ffdf75f611bf7b1f7d73e0
朝尘 <94768914+ZYQzhaochen@users.noreply.github.com>
2025-06-30 12:04:01 +0800

    Delete config directory

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//配置文件
-//其实目前并没有什么卵用，只是为日后的后端开发工作做个大致方向（画个大饼）
-$name="萌言网 - Moey";//网站名称
-$lang="cn";//>>(该功能暂时弃用)<<网站语言，支持en(英文)/cn（中文）
-$form="all";//网站首页句子类型，可选项可参考接口说明
-$foot="Copyright © <i>2023</i> <font color='#FF69B4'>萌云</font><br><a href='https://icp.gov.moe/?keyword=20230885' target='_blank'>萌ICP备 <img style='width:20px;height:20px;margin-bottom:5px;color: #FF69B4;' src='/images/moeicp.png'> 20230885号</a> | <a href='https://www.travellings.cn/go.html' target='_blank' rel='noopener' title='开往-友链接力'>「开往」</a>";//全局页脚内容
-$pw="1";//全局密钥，后端接口使用
-?>
